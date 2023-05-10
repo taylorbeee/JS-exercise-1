aa79796c4e1e41c7a212a729f549f322ee6e5133
Taylor Behrens <126136711+taylorbeee@users.noreply.github.com>
2023-05-10 16:39:20 -0700

    Initial commit

---
+++ script.js
@@ -0,0 +1,10 @@
+var string = "12";
+var number = Number(12);
+
+if (string === number) {
+  console.log(`They're equal!`);
+} else if (string == number) {
+  console.log(`True value!`);
+} else {
+  console.log(`Conditions not met`);
+}
